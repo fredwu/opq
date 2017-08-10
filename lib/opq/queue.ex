4ad79f59829b6d9d6dde3d31c788d78d1d11de54
Fred Wu <ifredwu@gmail.com>
2017-08-10 20:59:16 +1000

    Moved `Queue` to an agent so the state can be fetched

--- lib/opq/queue.ex
+++ lib/opq/queue.ex
@@ -7,44 +7,57 @@ defmodule OPQ.Queue do
   ## Examples
 
       iex> Queue.init
+      iex> Agent.get(:opq_queue, & &1)
       {[], []}
   """
-  defdelegate init, to: :queue, as: :new
+  def init do
+    Agent.start_link(fn -> :queue.new end, name: :opq_queue)
+  end
 
   @doc """
   ## Examples
 
       iex> Queue.init
-      iex> |> Queue.enqueue("hello")
+      iex> Queue.enqueue("hello")
       {["hello"], []}
 
       iex> Queue.init
-      iex> |> Queue.enqueue("hello")
-      iex> |> Queue.enqueue("world")
+      iex> Queue.enqueue("hello")
+      iex> Queue.enqueue("world")
       {["world"], ["hello"]}
   """
-  def enqueue(queue, item), do: :queue.in(item, queue)
+  def enqueue(item) do
+    queue = :queue.in(item, get_queue())
+
+    update_queue(queue)
+
+    queue
+  end
 
   @doc """
   ## Examples
 
       iex> Queue.init
-      iex> |> Queue.enqueue("hello")
-      iex> |> Queue.enqueue("world")
-      iex> |> Queue.dequeue
+      iex> Queue.enqueue("hello")
+      iex> Queue.enqueue("world")
+      iex> Queue.dequeue
       {"hello", {[], ["world"]}}
 
       iex> Queue.init
-      iex> |> Queue.enqueue("hello")
-      iex> |> Queue.dequeue
+      iex> Queue.enqueue("hello")
+      iex> Queue.dequeue
       {"hello", {[], []}}
 
       iex> Queue.init
-      iex> |> Queue.dequeue
+      iex> Queue.dequeue
       {:empty, {[], []}}
   """
-  def dequeue(queue) do
-    case :queue.out(queue) do
+  def dequeue do
+    queue = :queue.out(get_queue())
+
+    update_queue(queue)
+
+    case queue do
       {{:value, item}, new_queue} -> {item, new_queue}
       {:empty, new_queue}         -> {:empty, new_queue}
     end
@@ -54,13 +67,23 @@ defmodule OPQ.Queue do
   ## Examples
 
       iex> Queue.init
-      iex> |> Queue.is_empty?
+      iex> Queue.is_empty?
       true
 
       iex> Queue.init
-      iex> |> Queue.enqueue("hello")
-      iex> |> Queue.is_empty?
+      iex> Queue.enqueue("hello")
+      iex> Queue.is_empty?
       false
   """
-  defdelegate is_empty?(queue), to: :queue, as: :is_empty
+  def is_empty? do
+    :queue.is_empty(get_queue())
+  end
+
+  defp get_queue do
+    Agent.get(:opq_queue, & &1)
+  end
+
+  defp update_queue(new_queue) do
+    Agent.update(:opq_queue, fn(_) -> new_queue end)
+  end
 end
