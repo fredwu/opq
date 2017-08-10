91947bef50abc1d0e136263f7c78123a45637b4e
Fred Wu <ifredwu@gmail.com>
2017-08-10 21:01:46 +1000

    Configurable agent name for the queue

--- lib/opq/queue.ex
+++ lib/opq/queue.ex
@@ -9,9 +9,13 @@ defmodule OPQ.Queue do
       iex> Queue.init
       iex> Agent.get(:opq_queue, & &1)
       {[], []}
+
+      iex> Queue.init(:hello_queue)
+      iex> Agent.get(:hello_queue, & &1)
+      {[], []}
   """
-  def init do
-    Agent.start_link(fn -> :queue.new end, name: :opq_queue)
+  def init(name \\ :opq_queue) do
+    Agent.start_link(fn -> :queue.new end, name: name)
   end
 
   @doc """
