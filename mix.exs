1ebc078bc15ddaa3df5726be5c78167dcb31c59c
Fred Wu <ifredwu@gmail.com>
2017-08-30 22:37:25 +1000

    Release v2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OPQ.Mixfile do
   def project do
     [
       app:               :opq,
-      version:           "2.0.0",
+      version:           "2.0.1",
       elixir:            "~> 1.5",
       package:           package(),
       name:              "OPQ: One Pooled Queue",
